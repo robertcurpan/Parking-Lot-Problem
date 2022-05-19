57aad6c196713b04239b5868190cb7d64a2ef5e1
Robert Curpan <robert.curpan@basware.com>
2022-05-19 12:53:13 +0300

    Task IN-2: beautify variable names

--- ProblemaLocuriParcare/src/Driver.java
+++ ProblemaLocuriParcare/src/Driver.java
@@ -1,22 +1,22 @@
 public class Driver
 {
-    private String _name;
-    private VehicleType _vehicleType;
-    private boolean _vipStatus;
+    private String name;
+    private VehicleType vehicleType;
+    private boolean vipStatus;
 
     public Driver(String name, VehicleType vehicleType, boolean vipStatus)
     {
-        _name = name;
-        _vehicleType = vehicleType;
-        _vipStatus = vipStatus;
+        this.name = name;
+        this.vehicleType = vehicleType;
+        this.vipStatus = vipStatus;
     }
 
-    public String getName() { return _name; }
-    public VehicleType getVehicleType() { return _vehicleType; }
-    public boolean getVipStatus() { return _vipStatus; }
-    public void setName(String name) { _name = name; }
-    public void setVehicleType(VehicleType vehicleType) { _vehicleType = vehicleType; }
-    public void setVipStatus(boolean vipStatus) { _vipStatus = vipStatus; }
+    public String getName() { return name; }
+    public VehicleType getVehicleType() { return vehicleType; }
+    public boolean getVipStatus() { return vipStatus; }
+    public void setName(String name) { this.name = name; }
+    public void setVehicleType(VehicleType vehicleType) { this.vehicleType = vehicleType; }
+    public void setVipStatus(boolean vipStatus) { this.vipStatus = vipStatus; }
 
     @Override
     public String toString()
--- ProblemaLocuriParcare/src/ParkingLot.java
+++ ProblemaLocuriParcare/src/ParkingLot.java
@@ -4,34 +4,34 @@ import java.util.PriorityQueue;
 
 public class ParkingLot
 {
-    private int _smallDimension;
-    private int _mediumDimension;
-    private int _largeDimension;
-    private PriorityQueue<Integer>[] _parkingSpots;
-    private HashMap<Integer, Driver> _drivers;
+    private final int smallDimension;
+    private final int mediumDimension;
+    private final int largeDimension;
+    private PriorityQueue<Integer>[] parkingSpots;
+    private HashMap<Integer, Driver> drivers;
 
     public ParkingLot(int smallDimension, int mediumDimension, int largeDimension)
     {
-        _smallDimension = smallDimension;
-        _mediumDimension = mediumDimension;
-        _largeDimension = largeDimension;
+        this.smallDimension = smallDimension;
+        this.mediumDimension = mediumDimension;
+        this.largeDimension = largeDimension;
 
-        _parkingSpots = new PriorityQueue[3];
-        _parkingSpots[0] = new PriorityQueue<Integer>(_smallDimension);
-        _parkingSpots[1] = new PriorityQueue<Integer>(_mediumDimension);
-        _parkingSpots[2] = new PriorityQueue<Integer>(_largeDimension);
-        _drivers = new HashMap<Integer, Driver>();
+        parkingSpots = new PriorityQueue[3];
+        parkingSpots[0] = new PriorityQueue<Integer>(smallDimension);
+        parkingSpots[1] = new PriorityQueue<Integer>(mediumDimension);
+        parkingSpots[2] = new PriorityQueue<Integer>(largeDimension);
+        drivers = new HashMap<Integer, Driver>();
 
         // Initially, all the spots from the parking lot are empty
-        // Small -> [0, _smallDimension - 1]
-        // Medium -> [_smallDimension, _smallDimension + _mediumDimension - 1]
-        // Large -> [_smallDimension + _mediumDimension, _smallDimension + _mediumDimension + _largeDimension - 1]
-        for(int i = 0; i < _smallDimension; ++i)
-            _parkingSpots[0].add(i);
-        for(int i = _smallDimension; i < _smallDimension + _mediumDimension; ++i)
-            _parkingSpots[1].add(i);
-        for(int i = _smallDimension + _mediumDimension; i < _smallDimension + _mediumDimension + _largeDimension; ++i)
-            _parkingSpots[2].add(i);
+        // Small -> [0, smallDimension - 1]
+        // Medium -> [smallDimension, smallDimension + mediumDimension - 1]
+        // Large -> [smallDimension + mediumDimension, smallDimension + mediumDimension + largeDimension - 1]
+        for(int i = 0; i < smallDimension; ++i)
+            parkingSpots[0].add(i);
+        for(int i = smallDimension; i < smallDimension + mediumDimension; ++i)
+            parkingSpots[1].add(i);
+        for(int i = smallDimension + mediumDimension; i < smallDimension + mediumDimension + largeDimension; ++i)
+            parkingSpots[2].add(i);
     }
 
     // INSERT ( O(log n) )
@@ -44,15 +44,15 @@ public class ParkingLot
 
         // Check if there is any spot left in the driver's category (in minheap we store the id of the empty spots)
         int vehicleTypeId = d.getVehicleType().ordinal();
-        if(_parkingSpots[vehicleTypeId].size() != 0)        // We have an empty spot for this driver
+        if(parkingSpots[vehicleTypeId].size() != 0)        // We have an empty spot for this driver
         {
             try
             {
-                Integer idSpot = _parkingSpots[vehicleTypeId].peek();
-                _parkingSpots[vehicleTypeId].poll();
+                Integer idSpot = parkingSpots[vehicleTypeId].peek();
+                parkingSpots[vehicleTypeId].poll();
 
                 // Add the driver in the hashMap in order to map him to the id of the parking spot
-                _drivers.put(idSpot, d);
+                drivers.put(idSpot, d);
 
 
                 ans = "The driver " + d.toString() + " received the following parking slot: " + idSpot.toString();
@@ -72,13 +72,13 @@ public class ParkingLot
 
                 while(vehicleTypeId < 3 && !foundSpot)
                 {
-                    if(_parkingSpots[vehicleTypeId].size() != 0)
+                    if(parkingSpots[vehicleTypeId].size() != 0)
                     {
-                        Integer idSpot = _parkingSpots[vehicleTypeId].peek();
-                        _parkingSpots[vehicleTypeId].poll();
+                        Integer idSpot = parkingSpots[vehicleTypeId].peek();
+                        parkingSpots[vehicleTypeId].poll();
 
                         // Add the driver in the hashMap in order to map him to the id of the parking spot
-                        _drivers.put(idSpot, d);
+                        drivers.put(idSpot, d);
 
                         ans = "The driver " + d.toString() + " received the following parking slot: " + idSpot.toString();
                         foundSpot = true;
@@ -101,21 +101,21 @@ public class ParkingLot
     // DELETE ( O(log n) )
     public String leaveParkingLot(Integer idParkingSpot)
     {
-        if(!_drivers.containsKey(idParkingSpot))
+        if(!drivers.containsKey(idParkingSpot))
             return "Locul cu id-ul " + idParkingSpot.toString() + " nu este ocupat!";
 
         String ans = "-";
 
         // Add the place that has been freed
-        if(idParkingSpot >= 0 && idParkingSpot < _smallDimension)
-            _parkingSpots[0].add(idParkingSpot);
-        else if(idParkingSpot >= _smallDimension && idParkingSpot < _smallDimension + _mediumDimension)
-            _parkingSpots[1].add(idParkingSpot);
-        else if(idParkingSpot >= _smallDimension + _mediumDimension && idParkingSpot < _smallDimension + _mediumDimension + _largeDimension)
-            _parkingSpots[2].add(idParkingSpot);
+        if(idParkingSpot >= 0 && idParkingSpot < smallDimension)
+            parkingSpots[0].add(idParkingSpot);
+        else if(idParkingSpot >= smallDimension && idParkingSpot < smallDimension + mediumDimension)
+            parkingSpots[1].add(idParkingSpot);
+        else if(idParkingSpot >= smallDimension + mediumDimension && idParkingSpot < smallDimension + mediumDimension + largeDimension)
+            parkingSpots[2].add(idParkingSpot);
 
-        Driver dr = _drivers.get(idParkingSpot);
-        _drivers.remove(idParkingSpot);
+        Driver dr = drivers.get(idParkingSpot);
+        drivers.remove(idParkingSpot);
         ans = "The driver: " + dr.toString() + " has left the parking lot (he was on spot: " + idParkingSpot + ")";
 
         return ans;
@@ -125,16 +125,16 @@ public class ParkingLot
     {
         StringBuilder ans = new StringBuilder();
         ans.append("\r\n");
-        for(HashMap.Entry<Integer, Driver> entry : _drivers.entrySet())
+        for(HashMap.Entry<Integer, Driver> entry : drivers.entrySet())
         {
             ans.append(entry.getValue().toString()).append(" -> parking slot: ").append(entry.getKey().toString()).append("\r\n");
         }
 
         ans.append("\r\n");
         ans.append("-----> Number of free parking spots left: \r\n");
-        ans.append("Motorcycle: ").append(_parkingSpots[0].size()).append(" free spots.\r\n");
-        ans.append("Car: ").append(_parkingSpots[1].size()).append(" free spots.\r\n");
-        ans.append("Truck: ").append(_parkingSpots[2].size()).append(" free spots.\r\n");
+        ans.append("Motorcycle: ").append(parkingSpots[0].size()).append(" free spots.\r\n");
+        ans.append("Car: ").append(parkingSpots[1].size()).append(" free spots.\r\n");
+        ans.append("Truck: ").append(parkingSpots[2].size()).append(" free spots.\r\n");
 
         return ans.toString();
     }
